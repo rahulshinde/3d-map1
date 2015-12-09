f4d47ffd123305ac6f38c5a3d51f14bd20a9cd98
rahul shinde <rshinde@mica.edu>
2015-12-08 21:02:51 -0500

    adding new objects

--- js/environment.js
+++ js/environment.js
@@ -76,18 +76,18 @@ function init() {
 	shape1.bezierCurveTo( 28, 8, 30, 10, 32, 8 );
 	shape1.bezierCurveTo( 32, 8, 40, 0, 40, 0 );
 
-	var extrudeSetting1 = { amount: 10, bevelEnabled: true, bevelSegments: 4, steps: 3, bevelSize: 1, bevelThickness: 1 };
+	var extrudeSetting1 = { amount: 5, bevelEnabled: true, bevelSegments: 4, steps: 3, bevelSize: 1, bevelThickness: 1 };
 
 	var geometry1 = new THREE.ExtrudeGeometry( shape1, extrudeSetting1 );
 
 	var mesh1 = new THREE.Mesh( geometry1, new THREE.MeshPhongMaterial( { color: 0xffffff } ) );
 
-	mesh1.scale.set(1.5,1.5,1.5);
+	mesh1.scale.set(2.1,2.1,2.1);
 
 	mesh1.position.x = -10;
 	mesh1.position.z = 30;
 
-	// group.add( mesh1 );
+	group.add( mesh1 );
 
 	//shape2
 	var shape2 = new THREE.Shape();
@@ -95,19 +95,20 @@ function init() {
 	shape2.moveTo( -6, 0 );
 	shape2.bezierCurveTo( -6, 0, 20, 0, 20, 0 );
 	shape2.bezierCurveTo( 20, 0, 10, 6, 20, 22 );
-	shape2.bezierCurveTo( 30, 33, 35, 30, 45, 25 );
-	shape2.bezierCurveTo( 55, 10, 55, 10, 44, 8 );
-	shape2.bezierCurveTo( 45, 8, 60, 8, 70, 8 );
-	shape2.bezierCurveTo( 60, 8, 45, -45, 45, -45 );
-	shape2.bezierCurveTo( 45, -45, 15, -45, 15, -45 );
-	shape2.bezierCurveTo( 15, -45, 0, -20, -6, -20 );
+	shape2.bezierCurveTo( 30, 33, 40, 35, 52, 22 );
+	shape2.bezierCurveTo( 55, 17, 58, 3, 48, 7 );
+	shape2.bezierCurveTo( 48, 7, 70, 7, 80, 7 );
+	shape2.bezierCurveTo( 60, 7, 55, -35, 55, -45 );
+	shape2.bezierCurveTo( 55, -45, 15, -45, 15, -45 );
+	shape2.bezierCurveTo( 15, -35, 0, -20, -6, -20 );
 
-	var extrudeSetting2 = { amount: 20 };
+	var extrudeSetting2 = { amount: 15 };
 
 	var geometry2 = new THREE.ExtrudeGeometry( shape2, extrudeSetting2 );
 
 	var mesh2 = new THREE.Mesh( geometry2, new THREE.MeshPhongMaterial( { color: 0xffffff } ) );
 
+	mesh2.scale.set(0.4,0.4,0.4);
 	// mesh2.position.x = 20;
 	// mesh2.position.z = -40;
 	// mesh2.rotation.y = -0.9;
@@ -119,24 +120,51 @@ function init() {
 	var shape3 = new THREE.Shape();
 
 	shape3.moveTo( 0, 0 );
-	shape3.bezierCurveTo( 5, 5, 10, 5, 5, 5 );
-	shape3.bezierCurveTo( 6, 5, 6, 10, 0, 10 );
-	shape3.bezierCurveTo( -10, 10, -10, 12, -10, 8 );
-	shape3.bezierCurveTo( -10, 10, -40, -5, 5, -5 );
-	shape3.bezierCurveTo( 10, -10, 10, -25, 15, 5 );
+	shape3.bezierCurveTo( 0, 0, 3, -5, 15, -5 );
+	shape3.bezierCurveTo( 23, -5, 30, 0, 30, 0 );
+	shape3.bezierCurveTo( 30, 0, 30, -10, 30, -20 );
+	shape3.bezierCurveTo( 30, -30, 26, -27, 26, -27 );
+	shape3.bezierCurveTo( 26, -27, 26, -27, 24, -24 );
+	shape3.bezierCurveTo( 28, -15, 13, -10, 14, -24 );
+	shape3.bezierCurveTo( 15, -30, 22, -26, 21, -25 );
+	shape3.bezierCurveTo( 5, -50, 0, -24, 0, -24 );
 
 	var extrudeSetting3 = { amount: 5, bevelEnabled: true, bevelSegments: 4, steps: 3, bevelSize: 1, bevelThickness: 1 };
 
 	var geometry3 = new THREE.ExtrudeGeometry( shape3, extrudeSetting3 );
 
-	var mesh3 = new THREE.Mesh( geometry3, new THREE.MeshPhongMaterial( { color: 0xffffff, wireframe: true } ) );
+	var mesh3 = new THREE.Mesh( geometry3, new THREE.MeshPhongMaterial( { color: 0xffffff } ) );
 
-	mesh3.position.x = -10;
-	mesh3.position.z = -10;
-	mesh3.rotation.x = 0.5;
-	mesh3.rotation.z = 1.8;
+	mesh3.scale.set(1.5,1.5,1.5);
+	// mesh3.position.x = -10;
+	// mesh3.position.z = -10;
+	// mesh3.rotation.x = 0.5;
+	// mesh3.rotation.z = 1.8;
 
-	// group.add( mesh3 );
+	group.add( mesh3 );
+
+	//shape4
+	var shape4 = new THREE.Shape();
+
+	shape4.moveTo( 0, 0 );
+	shape4.bezierCurveTo( 0, 0, 20, 0, 22, 20 );
+	shape4.bezierCurveTo( 22, 20, 24, 7, 27, 11 );
+	shape4.bezierCurveTo( 31, 15, 32, 23, 37, 27 );
+	shape4.bezierCurveTo( 37, 27, 36, 9, 47, 0 );
+
+	var extrudeSetting4 = { amount: 5, bevelEnabled: true, bevelSegments: 4, steps: 3, bevelSize: 1, bevelThickness: 1 };
+
+	var geometry4 = new THREE.ExtrudeGeometry( shape4, extrudeSetting4 );
+
+	var mesh4 = new THREE.Mesh( geometry4, new THREE.MeshPhongMaterial( { color: 0xffffff } ) );
+
+	// mesh4.scale.set(1,1.5,1.5);
+	// mesh3.position.x = -10;
+	// mesh3.position.z = -10;
+	// mesh3.rotation.x = 0.5;
+	// mesh3.rotation.z = 1.8;
+
+	group.add( mesh4 );
 
 	scene.add( group );
 
